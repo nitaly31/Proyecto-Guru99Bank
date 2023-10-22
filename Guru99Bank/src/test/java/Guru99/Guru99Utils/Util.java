b4763776b2151993c6e0f4404003d340b2a4da38
Geraldine Nitaly Meneses <geraldine.nitaly@gmail.com>
2023-10-22 16:27:59 -0500

    feat(util): initialization parameters stored in separate file

---
+++ Guru99Bank/src/test/java/Guru99/Guru99Utils/Util.java
@@ -0,0 +1,11 @@
+package Guru99.Guru99Utils;
+
+public class Util {
+	public static final int WAIT_TIME = 30;
+	
+	public static final String USER_NAME = "mngr533450";
+	public static final String PWD = "arunAja";
+	
+	public static final String LOGIN_TITLE = "Guru99 Bank Home Page";
+	public static final String HOME_TITLE = "Guru99 Bank Manager HomePage";
+}
